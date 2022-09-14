6b858a911c77e1ddadbd8a9f204c7a9882d9c175
Kevin.Hernandez@cix.csi.cuny.edu <Kevin.Hernandez@cix.csi.cuny.edu>
2022-09-13 23:57:54 -0400

    Hello World program test pushed

---
+++ GroupProject/src/groupprojectpackage/GroupProjectClass.java
@@ -0,0 +1,10 @@
+package groupprojectpackage;
+
+public class GroupProjectClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Hello World");
+	}
+
+}
