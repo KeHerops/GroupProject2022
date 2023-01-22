e0b9c1cdd0acd063d901a385ef8f4de60ee4c6e5
kevinhernandez <kevinhernandez@localhost>
2023-01-22 13:38:54 -0500

    final touches before the presentation day which passed

--- GroupProject/gpsrc/groupprojectpackage/GameLogic.java
+++ GroupProject/gpsrc/groupprojectpackage/GameLogic.java
@@ -1,10 +1,25 @@
 package groupprojectpackage;
 
+import java.util.Scanner;
+
 public final class GameLogic {
+	
 private GameLogic() {}; // no no don't create an instance of this class
 
-
-
+public static Player chooseName() {
+	try  {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter a name: ");		
+		String name = sc.nextLine();
+		Player player = new Player(name, 0);
+		sc.close();
+		return player;
+	}
+	catch(Exception e) {
+		
+	}
+	return null;
+}
 
 
 }
